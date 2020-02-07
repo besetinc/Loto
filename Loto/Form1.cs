b27e490e7964e37deda1652c61e8de2d012cdd30
Bernard Šetinc <besetinc@vsite.hr>
2020-02-07 14:44:14 +0100

    Add draw next number code

--- Loto/Form1.cs
+++ Loto/Form1.cs
@@ -12,9 +12,71 @@ namespace Loto
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
+
+            HashSet<int> izvuceni = new HashSet<int>();
+            Random rand = new Random();
+            int klik = 0;
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                klik++;
+                switch (klik)
+                {
+                    case 1:
+                        int prvi = rand.Next(1, 37);
+                        textBox1.Text = prvi.ToString();
+                        izvuceni.Add(prvi);
+                        break;
+                    case 2:
+                        int drugi = rand.Next(1, 37);
+                        while (izvuceni.Contains(drugi))
+                        {
+                            drugi = rand.Next(1, 37);
+                        }
+                        textBox2.Text = drugi.ToString();
+                        izvuceni.Add(drugi);
+                        break;
+                    case 3:
+                        int treci = rand.Next(1, 37);
+                        while (izvuceni.Contains(treci))
+                        {
+                            treci = rand.Next(1, 37);
+                        }
+                        textBox3.Text = treci.ToString();
+                        izvuceni.Add(treci);
+                        break;
+                    case 4:
+                        int cetvrti = rand.Next(1, 37);
+                        while (izvuceni.Contains(cetvrti))
+                        {
+                            cetvrti = rand.Next(1, 37);
+                        }
+                        textBox4.Text = cetvrti.ToString();
+                        izvuceni.Add(cetvrti);
+                        break;
+                    case 5:
+                        int peti = rand.Next(1, 37);
+                        while (izvuceni.Contains(peti))
+                        {
+                            peti = rand.Next(1, 37);
+                        }
+                        textBox5.Text = peti.ToString();
+                        izvuceni.Add(peti);
+                        break;
+                    case 6:
+                        int sesti = rand.Next(1, 37);
+                        while (izvuceni.Contains(sesti))
+                        {
+                            sesti = rand.Next(1, 37);
+                        }
+                        textBox6.Text = sesti.ToString();
+                        izvuceni.Add(sesti);
+                        break;
+                }
+            }
         }
-    }
 }
